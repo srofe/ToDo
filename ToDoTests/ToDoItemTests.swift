7fbb7f733baf33f33de9fb2b542aa2f4b4732745
Simon Rofe <simon.rofe@ieee.org>
2022-07-27 12:39:40 +1000

    Refactor to remove redundancy
    The assertion was redundant since a non-failable initialiser cannot
    return nil. Also, as the result of the initialiser is not used, we just
    use the '_' to assign this result - which essentially means we ignore
    the result. Note: assigning to the underbar ('_') removes the compiler
    warning.

--- ToDoTests/ToDoItemTests.swift
+++ ToDoTests/ToDoItemTests.swift
@@ -10,7 +10,6 @@ import XCTest
 
 class ToDoItemTests: XCTestCase {
     func test_init_takesTitle() {
-        let item = ToDoItem(title: "Dummy")
-        XCTAssertNotNil(item, "The ToDo item shall not be nil.")
+        _ = ToDoItem(title: "Dummy")
     }
 }
