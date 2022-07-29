360d0cb440a056eae6f8680df2baa7fa436637d1
Simon Rofe <simon.rofe@ieee.org>
2022-07-29 13:56:52 +1000

    Add data source for table view
    A mock store is used for test purposes and this is used to send an item
    to the store. This item should be added to the table view in the view
    controller.

--- ToDo/ToDoItemsListViewController.swift
+++ ToDo/ToDoItemsListViewController.swift
@@ -9,4 +9,20 @@ import UIKit
 
 class ToDoItemsListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    var toDoItemStore: ToDoItemStorable?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+    }
+}
+
+extension ToDoItemsListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
 }
--- ToDoTests/ToDoItemsListViewControllerTests.swift
+++ ToDoTests/ToDoItemsListViewControllerTests.swift
@@ -10,10 +10,13 @@ import XCTest
 
 class ToDoItemsListViewControllerTests: XCTestCase {
     var sut: ToDoItemsListViewController!
+    var toDoItemStoreMock: ToDoItemStoreMock!
 
     override func setUpWithError() throws {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         sut = try XCTUnwrap(storyboard.instantiateInitialViewController() as? ToDoItemsListViewController, "The storyboard shall have a ToDoItemsListViewController.")
+        toDoItemStoreMock = ToDoItemStoreMock()
+        sut.toDoItemStore = toDoItemStoreMock
         sut.loadViewIfNeeded()
     }
 
@@ -27,5 +30,12 @@ class ToDoItemsListViewControllerTests: XCTestCase {
 
     func test_shouldHaveTableView() {
         XCTAssertTrue(sut.tableView.isDescendant(of: sut.view), "The ToDoItemsListViewController shall have a table view.")
+    }
+
+    func test_numberOfRows_whenOneItemIsSent_shouldReturnOne() {
+        toDoItemStoreMock.itemPublisher
+            .send([ToDoItem(title: "dummy 1")])
+        let result = sut.tableView.numberOfRows(inSection: 0)
+        XCTAssertEqual(result, 1, "The table view shall contain one item when one item added to the store.")
     }
 }
