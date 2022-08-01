59728d695609f27c6d068e90f763139bfbcec0af
Simon Rofe <simon.rofe@ieee.org>
2022-08-01 10:31:26 +1000

    Add the label to the cell's content view

--- ToDo/ToDoItemCell.swift
+++ ToDo/ToDoItemCell.swift
@@ -9,4 +9,13 @@ import UIKit
 
 class ToDoItemCell: UITableViewCell {
     let titleLabel = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titleLabel)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
---
+++ ToDoTests/ToDoItemCellTests.swift
@@ -0,0 +1,28 @@
+//
+//  ToDoItemCellTests.swift
+//  ToDoTests
+//
+//  Created by Simon Rofe on 1/8/2022.
+//
+
+import XCTest
+@testable import ToDo
+
+class ToDoItemCellTests: XCTestCase {
+    var sut: ToDoItemCell!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = ToDoItemCell()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+
+    func test_hasTitleLabelSubview() {
+        let subView = sut.titleLabel
+        XCTAssertTrue(subView.isDescendant(of: sut.contentView), "The ToDoItemCell shall have a title label view as a subview of its content.")
+    }
+}
