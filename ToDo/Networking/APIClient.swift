b015e17cde50bd07e8f356579ba7ee012961e001
Simon Rofe <simon.rofe@ieee.org>
2022-08-17 07:45:33 +1000

    Pass coordinate to completion handler for API client

--- ToDo/Networking/APIClient.swift
+++ ToDo/Networking/APIClient.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreLocation
 
 protocol APIClientProtocol {
-    func coordinate(for: String, completion: (Coordinate?) -> Void)
+    func coordinate(for: String, completion: @escaping (Coordinate?) -> Void)
 }
 
 protocol GeoCoderProtocol {
@@ -19,8 +19,14 @@ protocol GeoCoderProtocol {
 class APIClient: APIClientProtocol {
     lazy var geoCoder: GeoCoderProtocol = CLGeocoder()
 
-    func coordinate(for address: String, completion: (Coordinate?) -> Void) {
+    func coordinate(for address: String, completion: @escaping (Coordinate?) -> Void) {
         geoCoder.geocodeAddressString(address) { placemarks, error in
+            guard let clCoordinate = placemarks?.first?.location?.coordinate else {
+                completion(nil)
+                return
+            }
+            let coordinate = Coordinate(latitude: clCoordinate.latitude, longitude: clCoordinate.longitude)
+            completion(coordinate)
         }
     }
 }
--- ToDoTests/Networking/APIClientTests.swift
+++ ToDoTests/Networking/APIClientTests.swift
@@ -21,8 +21,16 @@ class APIClientTests: XCTestCase {
         sut = nil
     }
 
+    func test_coordinate_shouldCallGeoCoderWithAddress() {
+        let geocoderMock = GeoCoderProtocolMock()
+        sut.geoCoder = geocoderMock
+        let expectedAddress = "Dummy address"
+        sut.coordinate(for: expectedAddress) { _ in
+        }
+        XCTAssertEqual(geocoderMock.gecodeAddressString, expectedAddress, "When setting the coordinate the API client shall pass the address to the GeoCoder.")
+    }
+
     func test_coordinate_fetchesCoordinate() throws {
-        try XCTSkipIf(true, "Test to be split into two seperate tests: one for verifying if the method is called with the address string provided, and one for verifying the coordinate is passed into the completion closure.")
         let geoCoderMock = GeoCoderProtocolMock()
         sut.geoCoder = geoCoderMock
         let location = CLLocation(latitude: 1, longitude: 2)
@@ -33,17 +41,8 @@ class APIClientTests: XCTestCase {
             result = coordinate
         }
         geoCoderMock.completionHandler?([placemark], nil)
-        XCTAssertEqual(geoCoderMock.gecodeAddressString, expectedAddress)
-        XCTAssertEqual(result?.latitude, location.coordinate.latitude)
-        XCTAssertEqual(result?.longitude, location.coordinate.longitude)
-    }
-
-    func test_coordinate_shouldCallGeoCoderWithAddress() {
-        let geocoderMock = GeoCoderProtocolMock()
-        sut.geoCoder = geocoderMock
-        let expectedAddress = "Dummy address"
-        sut.coordinate(for: expectedAddress) { _ in
-        }
-        XCTAssertEqual(geocoderMock.gecodeAddressString, expectedAddress, "When setting the coordinate the API client shall pass the address to the GeoCoder.")
+        XCTAssertEqual(geoCoderMock.gecodeAddressString, expectedAddress, "When setting the coordinate the API client shall pass the address to the GeoCoder.")
+        XCTAssertEqual(result?.latitude, location.coordinate.latitude, "When setting the address, and a valid coordinate is retreived from the GeoCoder, the API client shall pass the coordinate to the completion handler - latitude.")
+        XCTAssertEqual(result?.longitude, location.coordinate.longitude, "When setting the address, and a valid coordinate is retreived from the GeoCoder, the API client shall pass the coordinate to the completion handler - longitude.")
     }
 }
